2ee7942861ccff1c5c50b1a74ff6e5b0112122b2
Byron Garcia <byron.garcia@tars.dev>
2021-04-27 11:56:20 -0500

    template config

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-// crear el servidor
+// crear el servidor new
 const app = express();
 
 // Conectar a la base de datos
+
 conectarDB();
 
 // habilitar cors
+
 app.use(cors());
 
 // Habilitar express.json
+
 app.use( express.json({ extended: true }));
 
 // puerto de la app
+
 const port = process.env.PORT || 4000;
 
 // Importar rutas
+
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
@@ -25,5 +30,5 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+    console.log(`Serever port: ${port}`);
 });
